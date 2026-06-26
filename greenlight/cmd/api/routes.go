b18b89e139945745c2836436f5282244bf4e9a89
khairul746 <khairullahilyas@gmail.com>
2026-06-26 12:07:36 +0700

    add recoverPanic middleware to handle panics in HTTP requests

--- greenlight/cmd/api/routes.go
+++ greenlight/cmd/api/routes.go
@@ -13,5 +13,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	return router
+	return app.recoverPanic(router)
 }
