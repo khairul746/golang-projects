8be72f0f8425c7941a5f4a58317bf2c5170a9dea
khairul746 <khairullahilyas@gmail.com>
2026-07-14 15:06:15 +0700

    add SortSafelist to Filters struct and update ValidateFilters for improved sorting validation

--- greenlight/internal/data/filters.go
+++ greenlight/internal/data/filters.go
@@ -1,7 +1,23 @@
 package data
 
+import (
+	"github.com/khairul746/golang-projects/greenlight/internal/validator"
+)
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafelist []string
+}
+
+// ValidateFilters checks the filters provided by the client to ensure they are valid.
+func ValidateFilters(v *validator.Validator, f Filters) {
+	// Check that the page and page_size parameters contain sensible values.
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be a maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
+	// Check that the sort parameter matches a value in the safelist.
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
